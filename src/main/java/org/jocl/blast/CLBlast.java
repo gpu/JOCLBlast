564f628b6f5a40cd08b1f22a1493e86bef632faa
Marco Hutter <jocl@jocl.org>
2021-01-24 19:17:34 +0100

    Updates for 1.5.1

--- src/main/java/org/jocl/blast/CLBlast.java
+++ src/main/java/org/jocl/blast/CLBlast.java
@@ -43,7 +43,7 @@ public class CLBlast
     // Initialization of the native library
     static
     {
-        String versionString = "1_5_0";
+        String versionString = "1_5_1";
         String libraryBaseName = "JOCLBlast_" + versionString;
         String libraryName = 
             LibUtils.createPlatformLibraryName(libraryBaseName);
